1801960d149a8c193b76603c7866b9f75256de57
Jeová Barbosa <jeovasousabarbosassj@gmail.com>
2023-03-28 22:55:38 -0300

    Ajuste de layout da janela de Cadastro de Produtos

--- interfaceCad.py
+++ interfaceCad.py
@@ -25,18 +25,31 @@ def função_principal():
 def chamarListaProdutos():
      interfaceList.show()
 
-cadastro = QLabel("Cadastro de Novo Produto", interfaceCad)
-cadastro.move(100,150) #eixo x, eixo y, largura e altura
-cadastro.setStyleSheet('font-size:15px')
+codigo = QLabel("Codigo do produto: ", interfaceCad)
+codigo.move(210,153) #eixo x, eixo y, largura e alt
+codigo.setStyleSheet('font-size:15px')
 
-LineEdit_Cod = QLineEdit("Código do Produto: ", interfaceCad)
-LineEdit_Cod.setGeometry(300,150,300,40)
-LineEdit_Desc = QLineEdit("Descrição: ", interfaceCad)
-LineEdit_Desc.setGeometry(300,200,300,40)
-LineEdit_Prec = QLineEdit("Preço por Unidade: ", interfaceCad)
-LineEdit_Prec.setGeometry(300,250,300,40)
-LineEdit_Quant = QLineEdit("Quantidade: ", interfaceCad)
-LineEdit_Quant.setGeometry(300,300,300,40)
+descricao = QLabel("Descrição do produto: ", interfaceCad)
+descricao.move(194,203) #eixo x, eixo y, largura e alt
+descricao.setStyleSheet('font-size:15px')
+
+preco = QLabel("Preço do produto: ", interfaceCad)
+preco.move(222,253) #eixo x, eixo y, largura e alt
+preco.setStyleSheet('font-size:15px')
+
+quantidade = QLabel("Quantidade: ", interfaceCad)
+quantidade.move(261,303) #eixo x, eixo y, largura e alt
+quantidade.setStyleSheet('font-size:15px')
+
+
+LineEdit_Cod = QLineEdit("", interfaceCad)
+LineEdit_Cod.setGeometry(350,150,150,30)
+LineEdit_Desc = QLineEdit("", interfaceCad)
+LineEdit_Desc.setGeometry(350,200,150,30)
+LineEdit_Prec = QLineEdit("", interfaceCad)
+LineEdit_Prec.setGeometry(350,250,150,30)
+LineEdit_Quant = QLineEdit("", interfaceCad)
+LineEdit_Quant.setGeometry(350,300,150,30)
 
 envio = QPushButton("Salvar", interfaceCad)
 envio.setGeometry(450, 500, 100, 50)
@@ -47,8 +60,8 @@ lista.setGeometry(250, 500, 100, 50)
 lista.clicked.connect(chamarListaProdutos)
 
 label = QLabel("Cadastro de Produtos", interfaceCad)
-label.move(300,45) #larg, alt
-label.setStyleSheet('font-size:20px')
+label.move(250,45) #larg, alt
+label.setStyleSheet('font-size:30px')
 
 interfaceCad.show() #mostrar tela
 
